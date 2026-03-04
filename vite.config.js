3967c59e2f3dcdb944509e96136c7f8eccdfb2eb
Mostafa Abdellatief - HomePC <mostafa.abdellatief@live.com>
2026-03-04 22:09:54 +0200

    run-dev

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/payband_site/',
   plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
+  server: {
+    open: true,
+  },
 })
